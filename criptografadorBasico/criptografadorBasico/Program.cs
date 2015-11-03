62bed3e4b03079d1d2c326b62ad2fc97ef473a4a
Wellington <wellington.schu@live.com>
2015-11-03 11:14:25 -0200

    First Submission

---
+++ criptografadorBasico/criptografadorBasico/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criptografadorBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criptografa: " + criptografa(31682, 24983));
+            Console.WriteLine("Decriptografa: " + decriptografa(791511406, 24983));
+
+            Console.ReadKey();
+
+        }
+
+        static int criptografa(int valor, int chave)
+        {
+            int numeroCriptografado;
+
+            return numeroCriptografado = valor * chave;
+        }
+
+        static int decriptografa(int valor, int chave)
+        {
+            int numeroDecriptografado;
+
+            return numeroDecriptografado = valor / chave;
+        }
+    }
+}
