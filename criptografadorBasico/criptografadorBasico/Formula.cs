c41e2389b2051a7d06cbed132c7ecb6725c1ffab
Wellington <wellington.schu@live.com>
2015-11-04 10:49:02 -0200

    Mudança de estruturado para orientado

---
+++ criptografadorBasico/criptografadorBasico/Formula.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criptografadorBasico
+{
+    class Formula
+    {
+
+        private int valor;
+        private int chave1;
+        private int chave2;
+        private int chave3;
+
+        public void setValor(int valor)
+        {
+            this.valor = valor;
+        }
+        public int getValor()
+        {
+            return valor;
+        }
+
+        public void setChave1(int chave1)
+        {
+            this.chave1 = chave1;
+        }
+
+        public int getChave1()
+        {
+            return chave1;
+        }
+
+        public void setChave2(int chave2)
+        {
+            this.chave2 = chave2;
+        }
+
+        public int getChave2()
+        {
+            return chave2;
+        }
+
+        public void setChave3(int chave3)
+        {
+            this.chave3 = chave3;
+        }
+
+        public int getChave3()
+        {
+            return chave3;
+        }
+
+        public int criptografa(int valor, int chave1, int chave2, int chave3)
+        {
+            int numeroCriptografado;
+
+            return numeroCriptografado = ((valor * chave1) / chave2) + chave3;
+        }
+
+        public int decriptografa(int valor, int chave1, int chave2, int chave3)
+        {
+            int numeroDecriptografado;
+
+            return numeroDecriptografado = ((valor - chave3) * chave2) / chave1;
+        }
+    }
+}
--- criptografadorBasico/criptografadorBasico/Program.cs
+++ criptografadorBasico/criptografadorBasico/Program.cs
@@ -10,25 +10,13 @@ namespace criptografadorBasico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Criptografa: " + criptografa(284, 50, 10, 5));
-            Console.WriteLine("Decriptografa: " + decriptografa(1425, 50, 10, 5));
+            Formula f = new Formula();
+
+            Console.WriteLine("Criptografa: " + f.criptografa(284, 50, 10, 5));
+            Console.WriteLine("Decriptografa: " + f.decriptografa(1425, 50, 10, 5));
 
             Console.ReadKey();
 
-        }
-
-        static int criptografa(int valor, int chave1, int chave2, int chave3)
-        {
-            int numeroCriptografado;
-
-            return numeroCriptografado = ((valor * chave1) / chave2) + chave3;
-        }
-
-        static int decriptografa(int valor, int chave1, int chave2, int chave3)
-        {
-            int numeroDecriptografado;
-
-            return numeroDecriptografado = ((valor - chave3) * chave2) / chave1;
         }
     }
 }
